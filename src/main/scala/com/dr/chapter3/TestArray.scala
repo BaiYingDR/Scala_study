7642d3ff938a624e1513ba9d783bb909f93c7e65
DavidDai <winterdie@163.com>
2023-05-12 23:08:29 +0800

    Array章节结束

--- src/main/scala/com/dr/chapter3/TestArray.scala
+++ src/main/scala/com/dr/chapter3/TestArray.scala
@@ -1,6 +1,7 @@
 package com.dr.chapter3
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConversions.bufferAsJavaList;
 
 object TestArray {
 
@@ -26,7 +27,7 @@ object TestArray {
     arrayBuffer.remove(2)
 
     // 在数组尾部追加所有值
-        arrayBuffer += (0, 7, 8, 9)
+    arrayBuffer += (0, 7, 8, 9)
 
     // ++= 操作符追加任何集合
     arrayBuffer ++= Array(8, 13, 21)
@@ -43,6 +44,51 @@ object TestArray {
     for (elem <- arrayBuffer) {
       println(elem)
     }
+
+    // 数组转换
+    // 转换动作不会修改原始数组，而是产生一个全新的数组
+
+    // 对于for() yield 循环，从什么出发，就会得到一个新的什么
+    //例如： 下面 变量a 是array， 那么得到的也是Array
+    // 如果a 是Arraybuffer， 那么得到的就是一个新的ArrayBuffer
+    val a = Array(2, 3, 5, 7, 11)
+    val result: Array[Int] = for (elem <- a) yield 2 * elem // Array(4, 6, 10, 14, 22)
+
+    // 在遍历一个集合时，可以使用条件判断处理
+    for (elem <- a if elem % 2 == 0) yield 2 * elem
+
+
+
+    //3.5 常用算法
+    println(Array(1, 2, 3, 4, 5).sum)
+    println(ArrayBuffer(1, 2, 3, 4, 5, 6).max)
+    println(ArrayBuffer(1, 2, 3, 4, 5, 6).min)
+
+    val b: ArrayBuffer[Int] = ArrayBuffer(1, 7, 2, 9)
+    val bSorted: ArrayBuffer[Int] = b.sorted // bSorted :ArrayBuffer(1,2,7,9)
+
+    val bSortedWith: ArrayBuffer[Int] = b.sortWith(_ > _)
+
+    // 可以对一个Array排序，但是不能对ArrayBuffer排序
+    scala.util.Sorting.quickSort(a)
+    //    scala.util.Sorting.quickSort(b)
+
+    println(a.mkString(" and ")) // 2 and 3 and 5 and 7 and 11
+
+    // 多维数组
+    val matrix: Array[Array[Double]] = Array.ofDim[Double](3, 4)
+    matrix(1)(3) = 42
+
+    //不规则的多维数组
+    val triangle = new Array[Array[Int]](10)
+    for (i <- 0 until triangle.length)
+      triangle(i) = new Array[Int](i + 1)
+
+
+    // 和 java 的互操作
+
+    val command = ArrayBuffer("ls", "-al", "/home/rudai")
+    val pb = new ProcessBuilder(command)
   }
 
 }
