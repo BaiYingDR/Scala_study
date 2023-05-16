6c6e016dd95d02169606ff70f277aa865d1ec471
DavidDai <winterdie@163.com>
2023-05-16 22:05:22 +0800

    单例对象、伴生对象、枚举

--- src/main/scala/com/dr/chapter6/SingleTon.scala
+++ src/main/scala/com/dr/chapter6/SingleTon.scala
@@ -1,7 +1,9 @@
 package com.dr.chapter6
 
-
-class SingleTon{
+/**
+ * 当需要用到既有实例方法又有静态方法的类时，可以使用伴生对象达到目的
+ */
+class SingleTon private(val id: Int, initialBalance: Double) {
 
 }
 /*
@@ -9,10 +11,22 @@ class SingleTon{
   或者想为其他值或者函数找一个可以挂靠的地方，就会用到这个
 
   scala中没有 static 关键字，所以不存在静态这个概念
-  可以用object 实现一样的功能
+  可以用 object 实现一样的功能
 
   该对象在执行newUniqueNumber方法时，同事调用构造器
   如果一个对象从未被调用，那么构造器也不会执行
+ */
+
+
+/**
+ * apply 方法： 通常一个apply方法返回的时伴生类的对象
+ * 当出现 object(param1 , ... , param2)这类调用时，会自动调用其apply方法, 通常直接返回的时一个伴生类的对象
+ * 例如： Array('David','Mary',...)
+ *
+ * Array(100) 和new Array(100)的区别在于:
+ * 前一个表达式调用的是apply(100)
+ * 后一个表达式调用的是构造器this(100)，结果是Array[Nothing]，包含100个null元素
+ *
  */
 object SingleTon {
   private var lastNumber = 0
@@ -21,4 +35,21 @@ object SingleTon {
     lastNumber += 1
     lastNumber
   }
+
+  def apply(initialBalance: Double) =
+    new SingleTon(newUniqueNumber(), initialBalance)
+}
+
+
+/**
+ * 枚举
+ * 枚举值的 ID 可通过 id() 方法返回， 名称通过 toString() 方法返回
+ */
+object TrafficLightColor extends Enumeration {
+//  val Red,Yellow,Green = Value
+
+  //如果不指定，则D 在将前一个枚举值基础上加一，从零开始，缺省名称为字段名
+  val Red = Value (0, "Stop")
+  val Yellow = Value (10) // 名称为Yellow
+  val Green = Value ("Go") // ID 为11
 }
